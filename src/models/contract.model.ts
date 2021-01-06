0902ad5eaf03626473e6e87cb63f2c5abb3555a1
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-06 21:57:49 +0100

    added schema for contract

--- src/models/contract.model.ts
+++ src/models/contract.model.ts
@@ -1,18 +1,104 @@
 import { Schema, Document, model, Types } from "mongoose";
-import { User, UserDoc } from "./user.model";
+import { User } from "./user.model";
+import { Dispute, DisputeDoc } from "./dispute.model";
 
 const ContractSchema = new Schema({
   promisor: {
-    type: Types.ObjectId,
-    ref: User,
+    id: {
+      type: Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    settled: {
+      type: Boolean,
+      default: false,
+    },
+    partyType: {
+      enum: ["Buyer", "Seller"],
+      required: true,
+    },
+    agreed: {
+      type: Boolean,
+      required: false,
+    },
   },
   promisee: {
-    type: Types.ObjectId,
-    ref: User,
+    id: {
+      type: Types.ObjectId,
+      ref: User,
+      required: true,
+    },
+    settled: {
+      type: Boolean,
+      default: false,
+    },
+    partyType: {
+      enum: ["Buyer", "Seller"],
+      required: true,
+    },
+    agreed: {
+      type: Boolean,
+      required: false,
+    },
+  },
+  deadline: {
+    type: Number,
+  },
+  token: {
+    type: Number,
+    min: 6,
+    max: 6,
+  },
+  initiatedAt: {
+    type: Date,
+    default: Date.now,
   },
   amountInvolved: {
-    type: Number,
+    type: Types.Decimal128,
+  },
+  itemInvolved: {
+    type: String,
+  },
+  disputes: {
+    type: [Dispute],
+  },
+  agreementReached: {
+    type: Boolean,
+    default: false,
+  },
+  resolved: {
+    type: Boolean,
+    default: false,
+  },
+  declined: {
+    type: Boolean,
+    dafault: false,
+  },
+  url: {
+    type: String,
   },
 });
 
-export default model("Contract", ContractSchema);
+export interface IContractPartyTypes {
+  id: string;
+  settled: boolean;
+  PartyType: string;
+  agreed: boolean;
+}
+
+export interface ContractDoc extends Document {
+  promisor?: IContractPartyTypes;
+  promisee?: IContractPartyTypes;
+  deadline?: number;
+  createdAt?: Date;
+  amountInvolved?: number;
+  itemInvolved?: string;
+  dispute?: Array<DisputeDoc>;
+  token?: number;
+  agreementReached?: boolean;
+  resolved?: boolean;
+  declined?: boolean;
+  url: string;
+}
+
+export const Contract = model<ContractDoc>("Contract", ContractSchema);
