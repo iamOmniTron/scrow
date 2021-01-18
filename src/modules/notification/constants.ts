8a16a6e301942add9b4437ef3895e33438585ea5
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-19 02:33:24 +0800

    enums and interfaces for notification

---
+++ src/modules/notification/constants.ts
@@ -0,0 +1,11 @@
+export enum Location{
+    PROFILE='profile',
+    ACCOUNT='account',
+    CONTRACT='contract'
+}
+
+export enum PriorityLevel{
+    NORMAL='normal',
+    PUSH='push',
+    MAIL='mail',
+}
