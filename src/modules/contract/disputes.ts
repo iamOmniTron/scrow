47c7eba085bc3f2bb307901f4b241812e9f8f321
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-18 16:08:49 +0100

    removed dipute file

--- src/modules/contract/disputes.ts
+++
@@ -1,7 +0,0 @@
-// import { DisputeActions } from "./contractPartyTypes";
-// import {Dispute,DisputeDoc} from "../../models/dispute.model"
-// export class Dispute{
-//   constructor(public name:string,public claim,public against:string,public requiredAction:DisputeActions){
-//
-//   }
-// }
