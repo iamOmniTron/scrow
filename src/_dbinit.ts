d02d6a07078a50a4562354b2f1b3d965099b6936
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-19 12:54:21 +0100

    changed mongoose connection string

--- src/_dbinit.ts
+++ src/_dbinit.ts
@@ -1,25 +1,18 @@
 import { connection, Mongoose } from "mongoose";
-import {connect} from "mongoose";
+import { connect } from "mongoose";
 import { MONGO_URI_DEV, MONGO_URI_PROD, NODE_ENV } from "./config/config";
 
-module.exports = function(){
-    const MONGO_URI = NODE_ENV === "development" ? MONGO_URI_PROD : MONGO_URI_DEV;
-    const db = connect(MONGO_URI_PROD, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-      });
-    
-      db.on('connect',()=>{
-          console.log(`connected to db in ${NODE_ENV} environment...`)
-      });
-
-      db.on('error',(error:Error)=>{
-        console.log(error.message);
-      });
-
-      db.on('disconnected',()=>{
-          console.log(`disonnecting from db...`)
-      })
-}
+module.exports = function () {
+  const MONGO_URI = NODE_ENV === "development" ? MONGO_URI_PROD : MONGO_URI_DEV;
+  connect(MONGO_URI_PROD, {
+    autoReconnect: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000,
+    keepAlive: true,
+    connectTimeoutMS: 30000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+};
