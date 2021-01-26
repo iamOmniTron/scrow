c4b2bf716578537e579e560b67943e94e2ba6f2d
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-26 15:59:04 +0100

    fixed db init error

--- src/_dbinit.ts
+++ src/_dbinit.ts
@@ -3,8 +3,8 @@ import { connect } from "mongoose";
 import { MONGO_URI_DEV, MONGO_URI_PROD, NODE_ENV } from "./config/config";
 
 module.exports = function () {
-  const MONGO_URI = NODE_ENV === "development" ? MONGO_URI_PROD : MONGO_URI_DEV;
-  connect(MONGO_URI_PROD, {
+  const MONGO_URI = NODE_ENV === "production" ? MONGO_URI_PROD : MONGO_URI_DEV;
+  connect(MONGO_URI, {
     autoReconnect: true,
     reconnectTries: Number.MAX_VALUE,
     reconnectInterval: 1000,
