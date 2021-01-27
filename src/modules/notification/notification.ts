2ea0b8f7cdfdcd4e66819d06b22db9f200c47116
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-27 10:50:09 +0800

    completed notificatiion functionality

--- src/modules/notification/notification.ts
+++ src/modules/notification/notification.ts
@@ -1,5 +1,6 @@
 import * as Pusher from "pusher"
 import {Events} from "./constants";
+import {Request} from "express";
 import {PUSHER_APP_ID,PUSHER_APP_KEY,PUSHER_SECRET,PUSHER_CLUSTER} from "../../config/config";
 
 const pusher = new Pusher({
@@ -9,7 +10,15 @@ const pusher = new Pusher({
     cluster: PUSHER_CLUSTER
 })
 export default class Notifier{
-    static async sendNotification(event:Events,exclude?:string){
-        
+    static async sendNotification(event:Events,payload:any,exclude?:string){
+        try {
+            // if(exclude){
+            //     pusher.trigger("notification",event,payload,req.headers['exclude'])
+            // }
+            //TODO:fix this
+            pusher.trigger('notification',event,payload)
+        } catch (error) {
+            throw new Error(error.message)
+        }
     }
 }
