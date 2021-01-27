9db69a01e555f0ed5cca3b2ebdddbc4935022894
abdulmumeen <abdulmumeen15500@gmail.com>
2021-01-27 10:28:51 +0800

    notification modified

--- src/modules/notification/constants.ts
+++ src/modules/notification/constants.ts
@@ -9,3 +9,14 @@ export enum PriorityLevel{
     PUSH='push',
     MAIL='mail',
 }
+
+export enum Events{
+    DELIVERY="delivery made",
+    PROMISEE_JOINS="promisee joins",
+    PROMISEE_AGREES="promisee agrees",
+    AGREEMENT_REACHED="agreement reached",
+    BUYER_PAID="buyer makes payment",
+    FUND_RELEASED="fund released",
+    CONTRACT_DECLINED="contract declined",
+    CONTRACT_RESOLVED="contract resolved",
+}
