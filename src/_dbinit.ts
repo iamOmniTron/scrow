c2dbdc4c58bf9903567e207d44cdd37bd093b5e4
abdulmumeen <abdulmumeen15500@gmail.com>
2021-02-04 09:33:56 +0100

    fixed deprecated messages

--- src/_dbinit.ts
+++ src/_dbinit.ts
@@ -5,14 +5,11 @@ import { MONGO_URI_DEV, MONGO_URI_PROD, NODE_ENV } from "./config/config";
 module.exports = function () {
   const MONGO_URI = NODE_ENV === "production" ? MONGO_URI_PROD : MONGO_URI_DEV;
   connect(MONGO_URI, {
-    autoReconnect: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000,
     keepAlive: true,
     connectTimeoutMS: 30000,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false,
-  });
+  }).then(() => console.log("connected to db"));
 };
