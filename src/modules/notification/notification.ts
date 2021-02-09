fd1d9889fba9cd307c6d644c18ea50e793f0d5ff
abdulmumeen <abdulmumeen15500@gmail.com>
2021-02-09 17:41:19 +0100

    fixed pusher import statement

--- src/modules/notification/notification.ts
+++ src/modules/notification/notification.ts
@@ -1,24 +1,29 @@
-import * as Pusher from "pusher"
-import {Events} from "./constants";
-import {Request} from "express";
-import {PUSHER_APP_ID,PUSHER_APP_KEY,PUSHER_SECRET,PUSHER_CLUSTER} from "../../config/config";
+import Pusher from "pusher";
+import { Events } from "./constants";
+import { Request } from "express";
+import {
+  PUSHER_APP_ID,
+  PUSHER_APP_KEY,
+  PUSHER_SECRET,
+  PUSHER_CLUSTER,
+} from "../../config/config";
 
 const pusher = new Pusher({
-    appId: PUSHER_APP_ID,
-    key: PUSHER_APP_KEY,
-    secret: PUSHER_SECRET,
-    cluster: PUSHER_CLUSTER
-})
-export default class Notifier{
-    static async sendNotification(event:Events,payload:any,exclude?:string){
-        try {
-            // if(exclude){
-            //     pusher.trigger("notification",event,payload,req.headers['exclude'])
-            // }
-            //TODO:fix this
-            pusher.trigger('notification',event,payload)
-        } catch (error) {
-            throw new Error(error.message)
-        }
+  appId: PUSHER_APP_ID,
+  key: PUSHER_APP_KEY,
+  secret: PUSHER_SECRET,
+  cluster: PUSHER_CLUSTER,
+});
+export default class Notifier {
+  static async sendNotification(event: Events, payload: any, exclude?: string) {
+    try {
+      // if(exclude){
+      //     pusher.trigger("notification",event,payload,req.headers['exclude'])
+      // }
+      //TODO:fix this
+      pusher.trigger("notification", event, payload);
+    } catch (error) {
+      throw new Error(error.message);
     }
+  }
 }
